e6fefc7842cccd9ea4b430762129e0837ad806c6
AbsoluteTiger <huzai9999999990@163.com>
2013-02-26 15:46:01 +0800

    rename weiborebot to playweibo

--- examples/weiborebot/dat/init_KR_TBL_GROUP.sql
+++
@@ -1,8 +0,0 @@
-begin;
-
-DELETE from KR_TBL_GROUP;
-
-insert into KR_TBL_GROUP (GROUP_ID, GROUP_NAME, GROUP_DESC, GROUP_STRING, GROUP_FUNC, GROUP_STATUS, REC_CRET_DTTM, LST_UPD_DTTM, LST_UPD_USER_ID)
-values (1, 'default_group', '默认群组', '(3 >= 1);', 'group_func', '1', '20120523141650   ', '20120523141650   ', 'Tiger');
-
-commit;
